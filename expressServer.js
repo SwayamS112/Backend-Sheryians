532a0ba7823ceb751ab7a7fc38758ce34ebb8036
SwayamS112 <soodswayam41@gmail.com>
2025-09-27 17:31:43 +0530

    Static file rendering CSS / JS

--- expressServer.js
+++ expressServer.js
@@ -9,17 +9,23 @@ app.use(morgan('dev'))
 // to get data from html files
 app.set('view engine','ejs') 
 
+// to render static files in backend from frontend like CSS / JS 
+app.use(express.static("public"))
+
+//used as build-in middlewares to get data from post method to body to get in console
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //this is a custom middleware
-app.use((req,res,next)=>{
-    console.log("this is middleware");
+// app.use((req,res,next)=>{
+//     console.log("this is middleware");
     
-    const a = 1;
-    const b = 2;
+//     const a = 1;
+//     const b = 2;
 
-    console.log(a+b);
-    return next()
-})
+//     console.log(a+b);
+//     return next()
+// })
 
 app.get("/",(req,res)=>{
      // render to show html page with index as a file name
@@ -28,19 +34,29 @@ app.get("/",(req,res)=>{
 
 //used custom middleware for a specific route 
 // this logic only works when /profile route hits
-app.get("/about",(req,res,next)=>{ 
-    const a=5;
-    const b=6;
-    console.log(a+b);
+// app.get("/about",(req,res,next)=>{ 
+//     const a=5;
+//     const b=6;
+//     console.log(a+b);
 
-    next();
-},(req,res)=>{
+//     next();
+// },(req,res)=>{
 
-    res.send("About page")
-})
+//     res.send("About page")
+// })
 
 app.get("/profile",(req,res)=>{
     res.send("Profile page")
+})
+
+// app.get("/get-form-data",(req,res)=>{
+//     console.log(req.query);
+//     res.send("Data received")
+// })
+
+app.post("/get-form-data",(req,res)=>{
+    console.log(req.body);
+    res.send("Data received")
 })
 
 app.listen(3000)
