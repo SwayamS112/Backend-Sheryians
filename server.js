2e5c83a01ea1f1e3d1bbaa864d3332bae2daa574
SwayamS112 <soodswayam41@gmail.com>
2025-09-28 11:25:06 +0530

    Crud mongo

--- server.js
+++ server.js
@@ -29,12 +29,15 @@ app.post("/register",async (req,res)=>{
     
 
 // to save the values from the input fields we do this below line of code
-    const {name , emailAddress , password , age , gender} = req.body
+    const {username , email , password , age , gender} = req.body
 //now get the data from the user inputs to mongo model
 // used async await function because this whole works take some time
+
+//CRUD ->
+// 1. Create
     await userModel.create({
-        username:name,
-        email:emailAddress,
+        username:username,
+        email:email,
         password:password,
         age:age,
         gender:gender
@@ -44,4 +47,44 @@ app.post("/register",async (req,res)=>{
     res.send("user register")
 })
 
+// 2. Read
+app.get("/get-users",(req,res)=>{
+    // returns all the data present inside user
+    // userModel.find().then((users)=>{
+    //    res.send(users)
+    // })
+  
+    // this is a condition only the username with 'b' will be printed
+
+    //  userModel.find({
+    //     username:'b'
+    //  }).then((users)=>{
+    //    res.send(users)
+    // })
+    
+    // findOne only returns one data 
+    userModel.findOne().then((users)=>{
+       res.send(users)
+    })
+})
+
+// 3. Update
+
+app.get('/update-user',async (req,res)=>{
+    await userModel.findOneAndUpdate({
+        username:"a"
+    },{
+        email:"c@c.com"
+    })
+    res.send("User Updated")
+})
+
+// 4. delete 
+
+app.get("/delete-user",async (req,res)=>{
+    await userModel.findOneAndDelete({
+        username:"a"
+    })
+    res.send("User Deleted")
+})
 app.listen(3000)
